e90d8840d303e6c58d8b3696c76acbb75668b49d
rem42 <me@remy.ovh>
2023-11-09 23:37:09 +0100

    feat: add extra compte

--- src/Model/Extra.php
+++ src/Model/Extra.php
@@ -5,6 +5,7 @@ namespace Scraper\ScraperDelivengo\Model;
 class Extra
 {
     private ?Module $module = null;
+    private ?Compte $compte = null;
 
     public function getModule(): ?Module
     {
@@ -14,6 +15,17 @@ class Extra
     public function setModule(?Module $module): self
     {
         $this->module = $module;
+        return $this;
+    }
+
+    public function getCompte(): ?Compte
+    {
+        return $this->compte;
+    }
+
+    public function setCompte(?Compte $compte): self
+    {
+        $this->compte = $compte;
         return $this;
     }
 }
