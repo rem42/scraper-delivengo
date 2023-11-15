f6567638446613f04a5a9fc46655a1c391abddd4
rem42 <me@remy.ovh>
2023-11-15 17:00:53 +0100

    feat: add code pays

--- src/Model/Destinataire.php
+++ src/Model/Destinataire.php
@@ -5,6 +5,7 @@ namespace Scraper\ScraperDelivengo\Model;
 class Destinataire extends Expediteur
 {
     private ?string $pays = null;
+    private ?string $codePays = null;
 
     public function getPays(): ?string
     {
@@ -14,6 +15,17 @@ class Destinataire extends Expediteur
     public function setPays(?string $pays): self
     {
         $this->pays = $pays;
+        return $this;
+    }
+
+    public function getCodePays(): ?string
+    {
+        return $this->codePays;
+    }
+
+    public function setCodePays(?string $codePays): self
+    {
+        $this->codePays = $codePays;
         return $this;
     }
 }
